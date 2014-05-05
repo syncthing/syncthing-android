b091d430a8d4a12589027f5e2bde3d75d2db63d5
Felix Ableitner <me@nutomic.com>
2014-05-05 22:30:57 +0200

    Create syncthing folder on start if it does not exist.

--- src/com/nutomic/syncthingandroid/MainActivity.java
+++ src/com/nutomic/syncthingandroid/MainActivity.java
@@ -2,11 +2,25 @@ package com.nutomic.syncthingandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
 
 public class MainActivity extends Activity {
 
+	private static final String TAG = "MainActivity";
+
+	private static final File SYNCTHING_FOLDER =
+			new File(Environment.getExternalStorageDirectory(), "syncthing");
+
     @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (!SYNCTHING_FOLDER.isDirectory() && !SYNCTHING_FOLDER.mkdir()) {
+			Log.w(TAG, "Failed to create syncthing folder on sdcard, exiting");
+			finish();
+			return;
+        }
 		System.loadLibrary("syncthing");
 	}
 	
