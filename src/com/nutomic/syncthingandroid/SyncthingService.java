472ccb6bed47420414f7f6eab93f092f0667c8f0
Felix Ableitner <me@nutomic.com>
2014-05-06 12:58:58 +0200

    Added persistent notification while service is running.

--- src/com/nutomic/syncthingandroid/SyncthingService.java
+++ src/com/nutomic/syncthingandroid/SyncthingService.java
@@ -1,5 +1,6 @@
 package com.nutomic.syncthingandroid;
 
+import android.app.Notification;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,8 +10,16 @@ public class SyncthingService extends Service {
 
 	private static final String TAG = "SyncthingService";
 
+	private static final int NOTIFICATION_ID = 1;
+
 	@Override
 	public void onCreate() {
+		Notification n = new Notification.Builder(this)
+				.setContentTitle(getString(R.string.app_name))
+				.setSmallIcon(R.drawable.ic_launcher)
+				.build();
+		startForeground(NOTIFICATION_ID, n);
+
 		new Thread(new Runnable() {
 			@Override
 			public void run() {
