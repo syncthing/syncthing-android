fd24e505111515c172e9952b0938ce4a298b76ac
Felix Ableitner <me@nutomic.com>
2014-05-18 15:18:42 +0200

    Use own build script instead of relying on syncthing build target.

---
+++ build-native.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd "$GOPATH/src/github.com/calmh/syncthing/"
+
+./build.sh test || exit 1
+./build.sh assets
+
+export GOOS=linux
+
+export GOARCH=386
+
+./build.sh
+cp syncthing syncthing-x86
+
+export GOARCH=arm
+
+export GOARM=7
+./build.sh
+cp syncthing syncthing-armeabi-v7a
+
+export GOARM=5
+./build.sh
+cp syncthing syncthing-armeabi
