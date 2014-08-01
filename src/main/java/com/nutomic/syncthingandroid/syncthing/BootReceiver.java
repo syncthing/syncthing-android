690190850e0cf168a92940711cea6fe1eb500266
Felix Ableitner <me@nutomic.com>
2014-08-01 23:21:37 +0200

    Start automatically on boot (closes #54).

---
+++ src/main/java/com/nutomic/syncthingandroid/syncthing/BootReceiver.java
@@ -0,0 +1,12 @@
+package com.nutomic.syncthingandroid.syncthing;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        context.startService(new Intent(context, SyncthingService.class));
+    }
+}
