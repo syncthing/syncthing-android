8061bfcc0c9a81ff1a55d05535f5c80e29e6765c
Felix Ableitner <me@nutomic.com>
2014-08-22 14:38:11 +0200

    Added unit tests for BroadcastReceivers.

--- src/main/java/com/nutomic/syncthingandroid/syncthing/BootReceiver.java
+++ src/main/java/com/nutomic/syncthingandroid/syncthing/BootReceiver.java
@@ -5,8 +5,10 @@ import android.content.Context;
 import android.content.Intent;
 
 public class BootReceiver extends BroadcastReceiver {
+
     @Override
     public void onReceive(Context context, Intent intent) {
         context.startService(new Intent(context, SyncthingService.class));
     }
+
 }
