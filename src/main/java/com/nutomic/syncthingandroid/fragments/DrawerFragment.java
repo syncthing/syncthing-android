cc74cd1fcfbbc51913c0375d8592162ac8f65360
Felix Ableitner <me@nutomic.com>
2016-04-26 23:08:35 +0200

    Fix crash in case DrawerFragment is opened early (fixes #629).

--- src/main/java/com/nutomic/syncthingandroid/fragments/DrawerFragment.java
+++ src/main/java/com/nutomic/syncthingandroid/fragments/DrawerFragment.java
@@ -125,7 +125,7 @@ public class DrawerFragment extends Fragment implements RestApi.OnReceiveSystemI
      * Invokes status callbacks.
      */
     private void updateGui() {
-        if (mActivity.getApi() == null || getActivity().isFinishing())
+        if (mActivity.getApi() == null || getActivity() == null || getActivity().isFinishing())
             return;
         mActivity.getApi().getSystemInfo(this);
         mActivity.getApi().getConnections(this);
