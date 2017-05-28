3fd14b845dfb87d17a66a6b3e4bbe3a531051211
Samarth Seksaria <samarth.s.seksaria@gmail.com>
2017-05-28 16:09:07 +0530

    Friendly message on android toolchain not found
    * Added a detailed message and reference links
    * Corrected indentation
    * Added handling for `x86` android builds as well. However, have commented the same, since, I do not have resources to test it. 
    
    Changes made based on comments from @AudriusButkevicius

--- make-syncthing.bash
+++ make-syncthing.bash
@@ -5,11 +5,30 @@ set -e
 MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 JNIDIR="/src/main/jniLibs"
 
+# The following android gcc executables can be found in the bin folder of your standalone android ndk toolchain
+armAndroidGcc="arm-linux-androideabi-gcc"
+arm64AndroidGcc="aarch64-linux-android-gcc"
+#x86AndroidGcc="x86-gcc"
+
+function checkAndroidToolchain() {
+        set +e
+        $1
+        if [ $? == 127 ]; then
+                printf "\nYou need to install a standalone android ndk toolchain to build Syncthing for Android. If you have already installed it, please add its bin folder to your PATH.\n\n"
+                echo "For a fresh installation refer the following links:"
+	        echo "    1. https://developer.android.com/ndk/guides/standalone_toolchain.html"
+	        echo "    2. https://gist.github.com/calmh/20c24afc283656695b76f7822c8c8997"
+	        exit 1
+        fi
+        set -e
+}
+
+
 case "$1" in
     arm)
         export CGO_ENABLED=1
-	# The executable "arm-linux-androideabi-gcc" can be found in the bin folder of your standalone android ndk toolchain
-	export CC=arm-linux-androideabi-gcc
+        checkAndroidToolchain $armAndroidGcc
+        export CC=$armAndroidGcc
         export GOOS=android
         export GOARCH=arm
         export GOARM=5
@@ -17,16 +36,18 @@ case "$1" in
         ;;
     arm64)
         export CGO_ENABLED=1
-	# The executable "aarch64-linux-android-gcc" can be found in the bin folder of your standalone android ndk toolchain
-	export CC=aarch64-linux-android-gcc
+        checkAndroidToolchain $arm64AndroidGcc
+        export CC=$arm64AndroidGcc
         export GOOS=android
         export GOARCH=arm64
         unset GOARM
         export TARGETDIR=$MYDIR$JNIDIR/arm64-v8a
         ;;
     386)
-        export CGO_ENABLED=0
-        export GOOS=linux
+        export CGO_ENABLED=0 # change this to 1
+        #checkAndroidToolchain $x86AndroidGcc
+        #export CC=$x86AndroidGcc
+        export GOOS=linux # change this to android
         export GOARCH=386
         export GO386=387
         export TARGETDIR=$MYDIR$JNIDIR/x86
@@ -63,4 +84,3 @@ fi
 popd
 
 echo "Build Complete"
-
