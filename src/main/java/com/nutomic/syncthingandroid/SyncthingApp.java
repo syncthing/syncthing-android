f4e1b077d3ec1449a297465653db700fe2d64fc2
Felix Ableitner <me@nutomic.com>
2017-09-04 02:07:04 +0900

    Move StrictMode handling into SyncthingApp class

--- src/main/java/com/nutomic/syncthingandroid/SyncthingApp.java
+++ src/main/java/com/nutomic/syncthingandroid/SyncthingApp.java
@@ -1,6 +1,7 @@
 package com.nutomic.syncthingandroid;
 
 import android.app.Application;
+import android.os.StrictMode;
 
 import com.nutomic.syncthingandroid.util.Languages;
 
@@ -10,5 +11,12 @@ public class SyncthingApp extends Application {
     public void onCreate() {
         super.onCreate();
         new Languages(this).setLanguage(this);
+
+        // Set VM policy to avoid crash when sending folder URI to file manager.
+        StrictMode.VmPolicy policy = new StrictMode.VmPolicy.Builder()
+                .detectAll()
+                .penaltyLog()
+                .build();
+        StrictMode.setVmPolicy(policy);
     }
 }
--- src/main/java/com/nutomic/syncthingandroid/activities/FirstStartActivity.java
+++ src/main/java/com/nutomic/syncthingandroid/activities/FirstStartActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -14,6 +13,7 @@ import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
 import com.nutomic.syncthingandroid.R;
 
 public class FirstStartActivity extends Activity implements Button.OnClickListener {
@@ -33,13 +33,6 @@ public class FirstStartActivity extends Activity implements Button.OnClickListen
         mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         Button cont = (Button) findViewById(R.id.cont);
         cont.setOnClickListener(this);
-
-        // Set VM policy to avoid crash when sending folder URI to file manager.
-        StrictMode.VmPolicy policy = new StrictMode.VmPolicy.Builder()
-                .detectAll()
-                .penaltyLog()
-                .build();
-        StrictMode.setVmPolicy(policy);
 
         if (!isFirstStart()) {
             if (haveStoragePermission()) {
