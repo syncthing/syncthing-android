32be8ba207f905eaeab7d7524f4c153961d39571
Felix Ableitner <me@nutomic.com>
2017-09-27 22:02:12 +0900

    Merged ToolbarActivity into SyncthingActivity

--- src/main/java/com/nutomic/syncthingandroid/activities/SyncthingActivity.java
+++ src/main/java/com/nutomic/syncthingandroid/activities/SyncthingActivity.java
@@ -4,20 +4,23 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.annimon.stream.Stream;
+import com.nutomic.syncthingandroid.R;
 import com.nutomic.syncthingandroid.service.RestApi;
 import com.nutomic.syncthingandroid.service.SyncthingService;
 import com.nutomic.syncthingandroid.service.SyncthingServiceBinder;
-import com.nutomic.syncthingandroid.util.Util;
 
 import java.util.LinkedList;
 
 /**
  * Connects to {@link SyncthingService} and provides access to it.
  */
-public abstract class SyncthingActivity extends ToolbarBindingActivity implements ServiceConnection {
+public abstract class SyncthingActivity extends AppCompatActivity implements ServiceConnection {
 
     public static final String EXTRA_FIRST_START = "com.nutomic.syncthing-android.SyncthingActivity.FIRST_START";
 
@@ -30,6 +33,26 @@ public abstract class SyncthingActivity extends ToolbarBindingActivity implement
      */
     public interface OnServiceConnectedListener {
         void onServiceConnected();
+    }
+
+    /**
+     * Look for a Toolbar in the layout and bind it as the activity's actionbar with reasonable
+     * defaults.
+     *
+     * The Toolbar must exist in the content view and have an id of R.id.toolbar. Trying to call
+     * getSupportActionBar before this Activity's onPostCreate will cause a crash.
+     */
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        if (toolbar == null)
+            return;
+
+        setSupportActionBar(toolbar);
+        //noinspection ConstantConditions
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
