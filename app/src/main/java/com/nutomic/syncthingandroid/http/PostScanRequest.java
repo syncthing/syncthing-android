4af56d7e9b92ce643db13d8e5d703541dd98a184
Catfriend1 <ref_github@reiseredv.de>
2018-03-14 16:38:36 +0100

    Prevent to post rooted sub to scanner rest api
    Remove the leading slash for syncthing versions <= 0.14.45 fixes #1032

--- app/src/main/java/com/nutomic/syncthingandroid/http/PostScanRequest.java
+++ app/src/main/java/com/nutomic/syncthingandroid/http/PostScanRequest.java
@@ -9,12 +9,16 @@ import com.google.common.collect.ImmutableMap;
 import java.net.URL;
 
 public class PostScanRequest extends ApiRequest {
-
     private static final String URI_SCAN = "/rest/db/scan";
 
     public PostScanRequest(Context context, URL url, String apiKey,
                            String folder, String sub) {
         super(context, url, URI_SCAN, apiKey);
+        
+        /* Prevent to post rooted sub to scanner rest api */
+        /* Remove the leading slash for syncthing versions <= 0.14.45 */
+        /* fixes #1032 */
+        sub = sub.replaceAll("^/+", "");
         Uri uri = buildUri(ImmutableMap.of("folder", folder, "sub", sub));
         connect(Request.Method.POST, uri, null, null, null);
     }
