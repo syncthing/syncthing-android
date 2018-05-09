1d6d8df1b92f33ddcda64faea37915049f11c062
Catfriend1 <Catfriend1@users.noreply.github.com>
2018-05-09 02:32:51 +0200

    add events

--- app/src/main/java/com/nutomic/syncthingandroid/service/EventProcessor.java
+++ app/src/main/java/com/nutomic/syncthingandroid/service/EventProcessor.java
@@ -183,6 +183,8 @@ public class EventProcessor implements SyncthingService.OnWebGuiAvailableListene
                 // Ignored.
                 break;
             case "DeviceConnected":
+            case "DeviceDisconnected":
+            case "DeviceDiscovered":
             case "DownloadProgress":
             case "FolderPaused":
             case "FolderScanProgress":
