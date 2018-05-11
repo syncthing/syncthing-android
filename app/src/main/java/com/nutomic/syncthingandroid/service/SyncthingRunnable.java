b59808e16389a7c3875bf9cd0319ff70d8aa77a5
Catfriend1 <Catfriend1@users.noreply.github.com>
2018-05-11 12:09:33 +0200

    revert import annotations in SyncthingRunnable

--- app/src/main/java/com/nutomic/syncthingandroid/service/SyncthingRunnable.java
+++ app/src/main/java/com/nutomic/syncthingandroid/service/SyncthingRunnable.java
@@ -1,6 +1,5 @@
 package com.nutomic.syncthingandroid.service;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
