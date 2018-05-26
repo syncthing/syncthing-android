ca26352a18f65d035777df26e94c5330cf1d2b2e
Catfriend1 <Catfriend1@users.noreply.github.com>
2018-05-26 13:45:06 +0200

    WIP - test build with onStop() in place

--- app/src/main/java/com/nutomic/syncthingandroid/activities/SettingsActivity.java
+++ app/src/main/java/com/nutomic/syncthingandroid/activities/SettingsActivity.java
@@ -442,15 +442,28 @@ public class SettingsActivity extends SyncthingActivity implements BackButtonHan
 
         @Override
         public boolean onBackClick() {
+            /*
             if (mRequireRestart) {
                 if (mSyncthingService.getCurrentState() != SyncthingService.State.DISABLED &&
                         mSyncthingService.getApi() != null) {
                     mSyncthingService.getApi().showRestartDialog(getActivity());
                 }
             }
+            */
 
             // Do not intercept the onBackPressed() handling of the parent activity.
             return false;
+        }
+
+        @Override
+        public void onStop() {
+            if (mRequireRestart) {
+                if (mSyncthingService.getCurrentState() != SyncthingService.State.DISABLED &&
+                        mSyncthingService.getApi() != null) {
+                    mSyncthingService.getApi().showRestartDialog(getActivity());
+                }
+            }
+            super.onStop();
         }
 
         /**
