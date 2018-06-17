3d94c3047ed83dbf6ac5ab7d04c81198c62e1ed6
Catfriend1 <Catfriend1@users.noreply.github.com>
2018-06-17 20:49:53 +0200

    remove unused import

--- app/src/main/java/com/nutomic/syncthingandroid/service/SyncthingService.java
+++ app/src/main/java/com/nutomic/syncthingandroid/service/SyncthingService.java
@@ -7,7 +7,6 @@ import android.content.SharedPreferences;
 import android.Manifest;
 import android.os.AsyncTask;
 import android.os.Handler;
-import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
