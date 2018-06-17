691edabaf618da01287ed10d2d0b64d7415d6642
Catfriend1 <ref_github@reiseredv.de>
2018-06-17 12:04:09 +0200

    Rename sttrace pref to debug_facilities_enabled (fixes #1133)

--- app/src/main/java/com/nutomic/syncthingandroid/service/SyncthingRunnable.java
+++ app/src/main/java/com/nutomic/syncthingandroid/service/SyncthingRunnable.java
@@ -432,7 +432,8 @@ public class SyncthingRunnable implements Runnable {
         HashMap<String, String> targetEnv = new HashMap<>();
         // Set home directory to data folder for web GUI folder picker.
         targetEnv.put("HOME", Environment.getExternalStorageDirectory().getAbsolutePath());
-        targetEnv.put("STTRACE", TextUtils.join(" ", mPreferences.getStringSet("sttrace", new HashSet<>())));
+        targetEnv.put("STTRACE", TextUtils.join(" ",
+                        mPreferences.getStringSet(Constants.PREF_DEBUG_FACILITIES_ENABLED, new HashSet<>())));
         File externalFilesDir = mContext.getExternalFilesDir(null);
         if (externalFilesDir != null)
             targetEnv.put("STGUIASSETS", externalFilesDir.getAbsolutePath() + "/gui");
