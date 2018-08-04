c9717224aa9500be3a5b396b48acd3135c0953bb
Catfriend1 <Catfriend1@users.noreply.github.com>
2018-08-04 19:42:23 +0200

    WIP - Add StatusFragment

---
+++ app/src/main/java/com/nutomic/syncthingandroid/fragments/StatusFragment.java
@@ -0,0 +1,58 @@
+package com.nutomic.syncthingandroid.fragments;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+
+import com.nutomic.syncthingandroid.R;
+import com.nutomic.syncthingandroid.activities.SyncthingActivity;
+import com.nutomic.syncthingandroid.service.Constants;
+import com.nutomic.syncthingandroid.service.SyncthingService;
+
+/**
+ * Displays why syncthing is running or disabled.
+ */
+public class StatusFragment extends ListFragment implements SyncthingService.OnServiceStateChangeListener {
+
+    private SyncthingService.State mServiceState = SyncthingService.State.INIT;
+
+    @Override
+    public void onServiceStateChange(SyncthingService.State currentState) {
+        mServiceState = currentState;
+        updateStatus();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_status, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        updateStatus();
+    }
+
+    private void updateStatus() {
+        SyncthingActivity activity = (SyncthingActivity) getActivity();
+        if (activity == null || getView() == null || activity.isFinishing()) {
+            return;
+        }
+
+        if (mServiceState == SyncthingService.State.ACTIVE) {
+            //setEmptyText("ToDo StatusFragment ACTIVE");
+        } else {
+            //setEmptyText("ToDo StatusFragment DISABLED");
+        }
+    }
+
+}
