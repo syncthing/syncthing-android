55915d79561d1ef111241273c40d768ba1fef63b
Catfriend1 <ref_github@reiseredv.de>
2018-09-15 20:16:20 +0200

    Fix crash when ignoring devices or folders (fixes #50) (#52)

---
+++ app/src/main/java/com/nutomic/syncthingandroid/model/FolderIgnoreList.java
@@ -0,0 +1,10 @@
+package com.nutomic.syncthingandroid.model;
+
+/**
+ * To avoid name confusion:
+ * This is the exclude and include items list associated with every folder.
+ */
+public class FolderIgnoreList {
+    public String[] expanded;
+    public String[] ignore;
+}
