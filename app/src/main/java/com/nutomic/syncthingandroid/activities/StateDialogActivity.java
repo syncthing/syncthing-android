338537e6595a91bfc6e7e8880fba067ef2cd4b7c
Catfriend1 <Catfriend1@users.noreply.github.com>
2020-05-17 15:17:39 +0200

    Migrate to AndroidX: import androidx.databinding.DataBindingUtil

--- app/src/main/java/com/nutomic/syncthingandroid/activities/StateDialogActivity.java
+++ app/src/main/java/com/nutomic/syncthingandroid/activities/StateDialogActivity.java
@@ -1,7 +1,7 @@
 package com.nutomic.syncthingandroid.activities;
 
 import android.content.Intent;
-import android.databinding.DataBindingUtil;
+import androidx.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
--- app/src/main/java/com/nutomic/syncthingandroid/views/FoldersAdapter.java
+++ app/src/main/java/com/nutomic/syncthingandroid/views/FoldersAdapter.java
@@ -2,7 +2,7 @@ package com.nutomic.syncthingandroid.views;
 
 import android.content.Context;
 import android.content.Intent;
-import android.databinding.DataBindingUtil;
+import androidx.databinding.DataBindingUtil;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
