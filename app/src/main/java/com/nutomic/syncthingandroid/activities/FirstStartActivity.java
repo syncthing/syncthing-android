a088701d51136785bd13451691ba36ea774c25a4
Bnyro <82752168+Bnyro@users.noreply.github.com>
2023-08-18 21:34:07 +0200

    Fix status bar text and icons color in light mode (fixes #1963) (#1965)
    closes #1963
    
    PS: This one has been kinda ugly to spot, ngl. I didn't expect that we
    were modifying the welcome activity status bar color programmatically
    instead of just with an XML theme ...

--- app/src/main/java/com/nutomic/syncthingandroid/activities/FirstStartActivity.java
+++ app/src/main/java/com/nutomic/syncthingandroid/activities/FirstStartActivity.java
@@ -93,12 +93,6 @@ public class FirstStartActivity extends Activity {
             return;
         }
 
-        // Make notification bar transparent (API level 21+)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        }
-
         // Show first start welcome wizard UI.
         setContentView(R.layout.activity_first_start);
         mViewPager = (ViewPager) findViewById(R.id.view_pager);
@@ -118,9 +112,6 @@ public class FirstStartActivity extends Activity {
         // Add bottom dots
         addBottomDots();
         setActiveBottomDot(0);
-
-        // Make notification bar transparent
-        changeStatusBarColor();
 
         mViewPagerAdapter = new ViewPagerAdapter();
         mViewPager.setAdapter(mViewPagerAdapter);
@@ -287,17 +278,6 @@ public class FirstStartActivity extends Activity {
 
         }
     };
-
-    /**
-     * Making notification bar transparent
-     */
-    private void changeStatusBarColor() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(Color.TRANSPARENT);
-        }
-    }
 
     /**
      * View pager adapter
