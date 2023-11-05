94bf1c3960147a5d8865e7aee926de104b4e92b1
André Colomb <src@andre.colomb.de>
2023-11-05 11:44:45 +0100

    Remove obsolete CLI flag specifying the resource ID.
    Ref. https://developers.transifex.com/docs/using-the-client#pulling-files-from-transifex

--- scripts/pull-translations.bash
+++ scripts/pull-translations.bash
@@ -8,10 +8,10 @@ printf "Pulling Translations
 # Force pull to make sure this is executed. Apparently tx only compares timestamps, not file
 # contents. So if a file was `touch`ed, it won't be updated by default.
 # Use multiple transifex instances for pulling to speed things up.
-tx pull -a -f -r "syncthing-android.stringsxml" &
-tx pull -a -f -r "syncthing-android.description_fulltxt" &
-tx pull -a -f -r "syncthing-android.description_shorttxt" &
-tx pull -a -f -r "syncthing-android.titletxt" &
+tx pull -a -f "syncthing-android.stringsxml" &
+tx pull -a -f "syncthing-android.description_fulltxt" &
+tx pull -a -f "syncthing-android.description_shorttxt" &
+tx pull -a -f "syncthing-android.titletxt" &
 wait
 ./gradlew deleteUnsupportedPlayTranslations
 git add -A "app/src/main/play/"
