be449b9f9f6bceca938a46b73ec5fe8da63afd61
Adam Szewera <adam.szewe@gmail.com>
2023-11-11 21:25:14 +0100

    Refactor the root gradle config to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+//    ext {
+//        ndkVersionShared = '25.2.9519653'
+//
+//    }
+    extra.apply {
+        // Cannot be called "ndkVersion" as that leads to naming collision
+        // Changes to this value must be reflected in `./docker/Dockerfile`
+        set("ndkVersionShared", "25.2.9519653")
+
+    }
+
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.3.1")
+        classpath("com.github.ben-manes:gradle-versions-plugin:0.36.0")
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
