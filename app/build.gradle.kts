64d2b8e362d45988dfc21dda3b0b494d7e8bb2c1
Simon Frei <freisim93@gmail.com>
2024-12-03 22:13:06 +0100

    Bumped version to 1.28.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.nutomic.syncthingandroid"
         minSdk = 21
         targetSdk = 33
-        versionCode = 4394
-        versionName = "1.28.0"
+        versionCode = 4395
+        versionName = "1.28.1"
         testApplicationId = "com.nutomic.syncthingandroid.test"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
